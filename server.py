b3feff9595ce1c2fa1b6ac2e80901a65f380d805
mcarreterob <m.carreterob@alumnos.urjc.es>
2016-10-22 19:07:58 +0200

    Terminado apartado 6

--- server.py
+++ server.py
@@ -8,19 +8,26 @@ import sys
 import socketserver
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegistrerHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+    data_client = {}
 
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
-        print(self.client_address)
-        for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+        print('IP client: ' + self.client_address[0])
+        print('PORT client: ' + str(self.client_address[1]))
+        cliente = self.rfile.read().decode('utf-8').split()
+        print(cliente[1])
+        if cliente[0] == 'REGISTRER':
+            self.data_client[cliente[1]] = self.client_address[0]
+            self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
+        #for line in self.rfile:
+       #     print("El cliente nos manda ", line.decode('utf-8'))
 
 if __name__ == "__main__":       # ip, puerto
-    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler)
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegistrerHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
