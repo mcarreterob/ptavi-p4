6bb6dc6f217cc844a1f8e85f9ef41f5f0479af32
mcarreterob <m.carreterob@alumnos.urjc.es>
2016-10-23 18:22:40 +0200

    Añadido IndexError a la introduccion de valores

--- client.py
+++ client.py
@@ -11,18 +11,22 @@ import socket
 #SERVER = 'localhost'
 #PORT = 6001
 #LINE = '¡Hola mundo!'
-
-ip = sys.argv[1]
-port = int(sys.argv[2])
-metodo = sys.argv[3]
-line = ' '.join(sys.argv[4:])
+try:
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+    metodo = sys.argv[3]
+    line = ' '.join(sys.argv[4:5])
+    expires = int(sys.argv[5])
+except IndexError:
+    sys.exit('Usage: client.py ip puerto register sip_address expires_value')
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
                    # tipo de red, tipo de paquete
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((ip, port))
-    if metodo == 'REGISTER':
-        line = 'REGISTER sip:' + line + ' SIP/2.0\r\n\r\n'
+    if metodo == 'register':
+        line = 'REGISTER sip:' + line + ' SIP/2.0\r\n' + 'Expires: ' + \
+               str(expires) + '\r\n\r\n'
     print("Enviando:", line)             # bytes
     my_socket.send(bytes(line, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
